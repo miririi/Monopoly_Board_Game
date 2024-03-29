2bc02095d385520530a73485ab6638217e6a1cde
miririi <wredemiriam@gmx.de>
2024-03-29 20:40:40 +0100

    test

--- src/main/java/org/example/Javafx.java
+++ src/main/java/org/example/Javafx.java
@@ -3,23 +3,34 @@ package org.example;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+
 public class Javafx extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+        @Override
+        public void start(Stage stage) {
+            String javaVersion = System.getProperty("java.version");
+            String javafxVersion = System.getProperty("javafx.version");
+            Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+            Scene scene = new Scene(new StackPane(l), 640, 480);
+            stage.setScene(scene);
+            stage.show();
 
-    public static void main(String[] args) {
-        launch();
-    }
+            /**BorderPane root = new BorderPane();
+            Scene scene = new Scene(root, 400, 400);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();**/
+
+
+        }
+
+        public static void main(String[] args) {
+            launch();
+        }
+
 
 }
