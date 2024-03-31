032d7d75200c113628060496d61e78949941b2f9
miririi <wredemiriam@gmx.de>
2024-03-31 22:02:34 +0200

    monopoly game board design ready

--- src/main/java/org/example/view/ViewManager.java
+++ src/main/java/org/example/view/ViewManager.java
@@ -1,17 +1,22 @@
 package org.example.view;
 
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-import org.example.model.Collect200Button;
-import org.example.model.FreeParkingButton;
-import org.example.model.GoJailButton;
-import org.example.model.JustVisitingButton;
 
 public class ViewManager {
     private AnchorPane mainPane;
     private Scene mainScene;
     private Stage mainStage;
+
+    private double screen_height;
 
     public ViewManager() {
         mainPane = new AnchorPane();
@@ -19,35 +24,64 @@ public class ViewManager {
         mainStage = new Stage();
         mainStage.setScene(mainScene);
         createButton();
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        //set Stage boundaries to visible bounds of the main screen
+        mainStage.setX(primaryScreenBounds.getMinX());
+        mainStage.setY(primaryScreenBounds.getMinY());
+        mainStage.setWidth(primaryScreenBounds.getWidth());
+        mainStage.setHeight(primaryScreenBounds.getHeight());
+        screen_height = primaryScreenBounds.getMaxY() - primaryScreenBounds.getMinY();
     }
 
     public Stage getMainStage() {
         return mainStage;
     }
 
+    private void setButtonPressedStyle(Button button, String style){  //Pressed Button is smaller
+        button.setStyle("-fx-background-color: transparent; -fx-background-image: url('"+ style + ".png');  -fx-background-size: 150px 150px; -fx-background-position: center; -fx-background-repeat: no-repeat");
+        button.setPrefHeight(145);
+        button.setLayoutY(button.getLayoutY()+4);
+
+    }
+
+    private void setButtonReleasedStyle(Button button, String style){
+        button.setStyle(style);
+        button.setPrefHeight(45);
+        button.setLayoutY(button.getLayoutY()-4);
+
+    }
+
+    private void initializeButtonListeners(String string, Button button){
+        button.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(event.getButton().equals(MouseButton.PRIMARY)) {
+                    setButtonPressedStyle(button, string);
+                }
+            }
+        });
+
+        button.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                mainPane.setEffect(new DropShadow());
+                ;            }
+        });
+    }
     private void createButton() {
-        FreeParkingButton free_parking_button = new FreeParkingButton();
-        JustVisitingButton just_visiting_button = new JustVisitingButton();
-        Collect200Button collect_200_button = new Collect200Button();
-        GoJailButton go_jail_button = new GoJailButton();
-
-
-
-        mainPane.getChildren().add(free_parking_button);
-        mainPane.getChildren().add(just_visiting_button);
-        mainPane.getChildren().add(collect_200_button);
-        mainPane.getChildren().add(go_jail_button);
-        /* button.setLayoutX(600);
-        collect_200_button.setLayoutY(500); */
-        mainPane.setTopAnchor(just_visiting_button, 0.0);
-        mainPane.setLeftAnchor(just_visiting_button, 0.0);
-        mainPane.setBottomAnchor(collect_200_button, 0.0);
-        mainPane.setLeftAnchor(collect_200_button, 0.0);
-        mainPane.setTopAnchor(free_parking_button, 0.0);
-        mainPane.setRightAnchor(free_parking_button, 0.0);
-        mainPane.setBottomAnchor(go_jail_button, 0.0);
-        mainPane.setRightAnchor(go_jail_button, 0.0);
-
+        double y_axis = 0.0;
+        String [] buttons = {"collect_200", "MR_avenue", "community_chest", "baltic_avenue", "income_tax", "reading_railroad", "oriental_avenue", "chance", "vermont_avenue", "connecticut_avenue", "just_visiting" };
+        for (String monopoly_button: buttons){
+            Button style_button = new Button();
+            style_button.setPrefHeight(120.5);
+            style_button.setPrefWidth(180);
+            style_button.setStyle("-fx-background-color: transparent; -fx-background-image: url('"+ monopoly_button + ".png');  -fx-background-size: 150px 150px; -fx-background-position: center; -fx-background-repeat: no-repeat");
+            mainPane.getChildren().add(style_button);
+            mainPane.setBottomAnchor(style_button, y_axis);
+            y_axis += 122.0;
+            initializeButtonListeners(monopoly_button, style_button);
+        }
 
 
     }
